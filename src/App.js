e3c039148a5c2f97a1cc46f3d78b34bbf1d21fc9
y0-gesh <yogeshtandan25@gmail.com>
2023-10-14 08:55:00 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Containts from './Components/Containts';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Containts />
+    </>
+  );
+}
+
+export default App;
